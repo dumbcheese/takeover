e7cbe06e4a0ced61ffa3d69e301577aa6433f802
unknown <damir.h552@gmail.com>
2019-08-16 19:39:19 +0200

    initializing

---
+++ app.js
@@ -0,0 +1,23 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+
+
+
+//ROUTES
+app.get("/", function(req, res){
+    res.render("index");
+});
+
+
+
+
+//SERVER
+app.listen("3000", function(){
+    console.log("takeover initiated, adi krpo ded");
+})
